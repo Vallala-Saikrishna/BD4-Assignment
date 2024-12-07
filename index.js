49022c88fc36ed985082a3e5d404695421e52fad
Vallala-Saikrishna <saikrishnavallala04@gmail.com>
2024-12-07 21:24:25 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,197 @@
+const express = require('express');
+let cors = require('cors');
+let sqlite3 = require('sqlite3').verbose();
+let { open } = require('sqlite');
+
+const app = express();
+const port = 3000;
+app.use(cors());
+app.use(express.json());
+
+let db;
+
+(async () => {
+  db = await open({
+    filename: './database.sqlite',
+    driver: sqlite3.Database,
+  });
+})();
+
+async function fetchAllRestaurants() {
+  let query = 'SELECT * FROM restaurants';
+  let response = await db.all(query, []);
+  return { restaurants: response };
+}
+app.get('/restaurants', async (req, res) => {
+  try {
+    let results = await fetchAllRestaurants();
+    if (results.restaurants.length === 0) {
+      return res.status(404).json({ message: 'No restaurants found.' });
+    }
+    res.status(200).json(results);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+async function fetchAllRestaurantsById(id) {
+  let query = 'SELECT * FROM restaurants WHERE id = ?';
+  let response = await db.all(query, [id]);
+  return { restaurants: response };
+}
+app.get('/restaurants/details/:id', async (req, res) => {
+  try {
+    let id = req.params.id;
+    let results = await fetchAllRestaurantsById(id);
+    if (results.restaurants.length === 0) {
+      return res
+        .status(404)
+        .json({ message: 'No restaurants found by this id.' });
+    }
+    res.status(200).json(results);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+async function fetchAllRestaurantsByCuisine(cuisine) {
+  let query = 'SELECT * FROM restaurants WHERE cuisine = ?';
+  let response = await db.all(query, [cuisine]);
+  return { restaurants: response };
+}
+
+app.get('/restaurants/cuisine/:cuisine', async (req, res) => {
+  try {
+    let cuisine = req.params.cuisine;
+    let results = await fetchAllRestaurantsByCuisine(cuisine);
+    if (results.restaurants.length === 0) {
+      return res
+        .status(404)
+        .json({ message: 'No restaurants found by this cuisine.' });
+    }
+    res.status(200).json(results);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+async function filterRestaurants(isVeg, hasOutdoorSeating, isLuxury) {
+  let query =
+    'SELECT * FROM restaurants WHERE isVeg = ? AND  hasOutdoorSeating = ? AND isLuxury = ?';
+  let response = await db.all(query, [isVeg, hasOutdoorSeating, isLuxury]);
+  return { restaurants: response };
+}
+
+app.get('/restaurants/filter', async (req, res) => {
+  try {
+    let isVeg = req.query.isVeg;
+    let hasOutdoorSeating = req.query.hasOutdoorSeating;
+    let isLuxury = req.query.isLuxury;
+    let results = await filterRestaurants(isVeg, hasOutdoorSeating, isLuxury);
+    if (results.restaurants.length === 0) {
+      return res
+        .status(404)
+        .json({ message: 'No restaurants found by this filter.' });
+    }
+    res.status(200).json(results);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+async function SortAllRestaurantsByRating() {
+  let query = 'SELECT * FROM restaurants ORDER BY rating DESC';
+  let response = await db.all(query, []);
+  return { restaurants: response };
+}
+
+app.get('/restaurants/sort-by-rating', async (req, res) => {
+  try {
+    let results = await SortAllRestaurantsByRating();
+    if (results.restaurants.length === 0) {
+      return res.status(404).json({ message: 'No restaurants found.' });
+    }
+    res.status(200).json(results);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+async function FetchAllDishes() {
+  let query = 'SELECT * FROM dishes';
+  let response = await db.all(query, []);
+  return { dishes: response };
+}
+
+app.get('/dishes', async (req, res) => {
+  try {
+    let results = await FetchAllDishes();
+    if (results.dishes.length === 0) {
+      return res.status(404).json({ message: 'No dishes found.' });
+    }
+    res.status(200).json(results);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+async function FetchAllDishesById(id) {
+  let query = 'SELECT * FROM dishes WHERE id= ?';
+  let response = await db.all(query, [id]);
+  return { dishes: response };
+}
+
+app.get('/dishes/details/:id', async (req, res) => {
+  try {
+    let id = req.params.id;
+    let results = await FetchAllDishesById(id);
+    if (results.dishes.length === 0) {
+      return res.status(404).json({ message: 'No dishes found by this Id.' });
+    }
+    res.status(200).json(results);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+async function FilterAllDishesByCategory(isVeg) {
+  let query = 'SELECT * FROM dishes WHERE isVeg = ?';
+  let response = await db.all(query, [isVeg ? 'true' : 'false']);
+  return { dishes: response };
+}
+
+app.get('/dishes/filter', async (req, res) => {
+  try {
+    let isVeg = req.query.isVeg === 'true';
+    let results = await FilterAllDishesByCategory(isVeg);
+    if (results.dishes.length === 0) {
+      return res
+        .status(404)
+        .json({ message: 'No dishes found by this filter.' });
+    }
+    res.status(200).json(results);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+async function SortAllDishesByPrice() {
+  let query = 'SELECT * FROM dishes ORDER BY price ASC';
+  let response = await db.all(query, []);
+  return { dishes: response };
+}
+
+app.get('/dishes/sort-by-price', async (req, res) => {
+  try {
+    let results = await SortAllDishesByPrice();
+    if (results.dishes.length === 0) {
+      return res.status(404).json({ message: 'No dishes found.' });
+    }
+    res.status(200).json(results);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
