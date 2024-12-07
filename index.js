36dff2b9f055e8d671082657c509cd9509ea40a5
Vallala-Saikrishna <saikrishnavallala04@gmail.com>
2024-12-07 22:12:14 +0530

    updated index.js

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express');
 let cors = require('cors');
 let sqlite3 = require('sqlite3').verbose();
 let { open } = require('sqlite');
-const { path } = require('express/lib/application');
+const path = require('path'); 
 
 const app = express();
 const port = 3000;
 app.use(cors());
 app.use(express.json());
+
 
 let db;
 
